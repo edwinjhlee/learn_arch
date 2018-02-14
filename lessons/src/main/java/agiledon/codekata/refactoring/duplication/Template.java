e5a4411c7520c35c674c45d32fbce4a8b449ef39
EdwinJHLee <edwin.jh.lee@gmail.com>
2018-02-14 13:07:33 +0800

    Fix duplication code

--- lessons/src/main/java/agiledon/codekata/refactoring/duplication/Template.java
+++ lessons/src/main/java/agiledon/codekata/refactoring/duplication/Template.java
@@ -4,29 +4,29 @@ import java.io.PrintStream;
 
 public class Template {
 
+
+    public static String getCode(String template, int templateSplitBegin, int templateSplitEnd, String code){
+        String templatePartOne = new String(template.substring(0, templateSplitBegin));
+        String templatePartTwo =
+                new String(template.substring(templateSplitEnd, template.length()));
+        return new String(templatePartOne + code + templatePartTwo);
+    }
+
     public void applyTemplate(String sourceTemplate, String reqId, PrintStream out) {
-        String code;
-        String altcode;
 
         try {
             String template = new String(sourceTemplate);
 
             // Substitute for %CODE%
             int templateSplitBegin = template.indexOf("%CODE%");
-            int templateSplitEnd = templateSplitBegin + 6;
-            String templatePartOne = new String(template.substring(0, templateSplitBegin));
-            String templatePartTwo =
-                    new String(template.substring(templateSplitEnd, template.length()));
-            code = new String(reqId);
-            template = new String(templatePartOne + code + templatePartTwo);
+            String code = new String(reqId);
+            template = getCode(template, templateSplitBegin, templateSplitBegin + 6, code);
 
             // Substitute for %ALTCODE%
-            templateSplitBegin = template.indexOf("%ALTCODE%");
-            templateSplitEnd = templateSplitBegin + 9;
-            templatePartOne = new String(template.substring(0, templateSplitBegin));
-            templatePartTwo = new String(template.substring(templateSplitEnd, template.length()));
-            altcode = code.substring(0, 5) + "-" + code.substring(5, 8);
-            out.print(templatePartOne + altcode + templatePartTwo);
+            final int altcode_templateSplitBegin = template.indexOf("%ALTCODE%");
+            final String tmp2 = getCode(template, altcode_templateSplitBegin, altcode_templateSplitBegin+9, code.substring(0, 5) + "-" + code.substring(5, 8);)
+
+            out.print(tmp2);
         } catch (Exception e) {
             System.out.println("Error in substitute()");
         }

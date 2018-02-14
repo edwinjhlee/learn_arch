219fd972723e0f88eea541a6a18d2ea91058ed86
EdwinJHLee <edwin.jh.lee@gmail.com>
2018-02-14 12:55:56 +0800

    Paperboy fixed

--- lessons/src/main/java/agiledon/codekata/refactoring/demeter/Paperboy.java
+++ lessons/src/main/java/agiledon/codekata/refactoring/demeter/Paperboy.java
@@ -1,14 +1,25 @@
 package agiledon.codekata.refactoring.demeter;
 
 public class Paperboy {
-    private Customer myCustomer;
+    //    private Customer myCustomer;
 
-    public void pay(float payment) {
-        Wallet theWallet = myCustomer.getWallet();
-        if (theWallet.getTotalMoney() > payment) {
-            theWallet.subtractMoney(payment);
-        } else {
-            //money not enough
-        }
+    //    public void pay(float payment) {
+    //        Wallet theWallet = myCustomer.getWallet();
+    //        if (theWallet.getTotalMoney() > payment) {
+    //            theWallet.subtractMoney(payment);
+    //        } else {
+    //            //money not enough
+    //        }
+    //    }
+
+    private Wallet wallet = new Wallet();
+
+    public boolean getPaid(Customer customer, float payment) {
+        final Wallet w = customer.getWallet();
+        if (w.getTotalMoney() < payment) return false;
+
+        w.subtractMoney(payment);
+        this.wallet.addMoney(payment);
+        return true;
     }
 }
